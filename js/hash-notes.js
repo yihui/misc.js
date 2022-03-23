95519f3e1ee996d4860d1704e9a4ffa3c969a9d8
Yihui Xie <xie@yihui.name>
2022-03-23 00:13:04 -0500

    correct 1ceb20608a78bc4dd4dff5f172aecbfb30fb8fc2

--- js/hash-notes.js
+++ js/hash-notes.js
@@ -10,8 +10,7 @@
     s.innerText = t.replace(r, '$1');
     s.innerHTML = s.innerHTML
       .replace(/\[([^\]]+)\]\(([^)]+)\)/g, '<a href="$2" target="_blank">$1</a>')
-      .replace(/(?<!")(https?:\/\/)([-a-zA-Z0-9%._#=/\+]+)/g, '<a href="$1$2" target="_blank">$2</a>')
-      .replace(/(>[^#]+#)([^<]+)(<\/a>)/g, '$1$3');
+      .replace(/(?<!")(https?:\/\/)([-a-zA-Z0-9%._=/\+]+)(#)?([-a-zA-Z0-9%._=\+]+)?/g, '<a href="$1$2$3$4" target="_blank">$2$3</a>');
     el.before(s);
     el.remove();
   };
