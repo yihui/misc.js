d8141b596ac60f0b994fffb6e112ccf690014a16
Yihui Xie <xie@yihui.name>
2022-07-01 22:59:29 -0500

    加一句提示语

--- js/toggle-notes.js
+++ js/toggle-notes.js
@@ -2,11 +2,13 @@
   if (!d.body.classList.contains('has-notes')) return;
   const h = d.querySelector('.title > hr');
   if (!h) return;
+  const s = sessionStorage.getItem('hide-notes');
   h.classList.add('toggle-notes');
   h.onclick = function(e) {
-    const s = d.body.classList.toggle('hide-notes');
+    s === null && !/^(localhost|[0-9.]+)$/.test(location.hostname) &&
+      alert('你好像发现了个什么不得了的机关……请勿公开，自行享用即可，谢谢！');
+    s = d.body.classList.toggle('hide-notes');
     try { sessionStorage.setItem('hide-notes', s); } catch (e) {};
   };
-  const s = sessionStorage.getItem('hide-notes');
   if (s !== null) d.body.classList.toggle('hide-notes', s === 'true');
 })(document);
