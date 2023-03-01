e38cf404cb4e27bbf2a9a288008f4ba096c0abed
Yihui Xie <xie@yihui.name>
2023-03-01 11:20:40 -0600

    add [] to footnote numbers, and delete the function fix_footnotes() was only for Hugo's Blackfriday, which I no longer use

--- js/fix-footnote.js
+++ js/fix-footnote.js
@@ -1,30 +1,8 @@
 (function(d) {
-  function fix_footnote(tagName) {
-    let tags = d.getElementsByTagName(tagName), i, tag, tag2, n = 1, href;
-    for (i = 0; i < tags.length; i++) {
-      tag = tags[i];
-      if (tagName === 'sup') {
-        if (tag.className !== 'footnote-ref' || tag.id !== 'fnref:-') continue;
-        tag.id += n;
-        if (tag.children.length === 1) {
-          tag2 = tag.children[0];
-          href = tag2.getAttribute('href');
-          if (tag2.nodeName === 'A' && href === '#fn:-') {
-            tag2.setAttribute('href', href + n);
-          }
-        }
-      } else if (tagName === 'li') {
-        if (tag.id !== 'fn:-') continue;
-        tag.id += n;
-        tag2 = tag.lastChild; href = tag2.getAttribute('href');
-        if (tag2.nodeName === 'A' && href === '#fnref:-') {
-          tag2.setAttribute('href', href + n);
-        }
-      }
-      n++;
-    }
-  }
-  fix_footnote('sup'); fix_footnote('li');
+  // add [] to footnote numbers
+  d.querySelectorAll('sup[id^="fnref:"] > a.footnote-ref, a.footnote-ref > sup').forEach(el => {
+    if (/^[0-9]+$/.test(el.innerText)) el.innerText = ' [' + el.innerText + ']';
+  });
   // move the return symbol into the previous <p>
   d.querySelectorAll('.footnotes > ol > li > p ~ .footnote-return').forEach(el => {
     el.previousElementSibling.lastChild.after(el);
