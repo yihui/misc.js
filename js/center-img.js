c135d7c553b9f65a32a738fdb50d32e461b598d9
Yihui Xie <xie@yihui.name>
2023-03-03 13:45:55 -0600

    there may be text nodes in the paragraph; center the image only if all text nodes are blank

--- js/center-img.js
+++ js/center-img.js
@@ -1,16 +1,26 @@
 (function() {
+  function one_child(el) {
+    if (el.childElementCount !== 1) return false;
+    const nodes = el.childNodes;
+    if (nodes.length === 1) return true;
+    for (let i in nodes) {
+      let node = nodes[i];
+      if (node.nodeName === '#text' && !/^\s$/.test(node.textContent)) return false;
+    }
+    return true;
+  }
   function center_el(tagName) {
     var tags = document.getElementsByTagName(tagName), i, tag;
     for (i = 0; i < tags.length; i++) {
       tag = tags[i];
       var parent = tag.parentElement;
       // center an image if it is the only element of its parent
-      if (parent.childElementCount === 1) {
+      if (one_child(parent)) {
         // if there is a link on image, check grandparent
         var parentA = parent.nodeName === 'A';
         if (parentA) {
           parent = parent.parentElement;
-          if (parent.childElementCount != 1) continue;
+          if (!one_child(parent)) continue;
           parent.firstElementChild.style.border = 'none';
         }
         if (parent.nodeName === 'P') {
