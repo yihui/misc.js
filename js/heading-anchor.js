aa0d03c03514b36fd18e28436bdcfd7d41f64389
Yihui Xie <xie@yihui.name>
2023-03-31 15:46:48 -0500

    support all levels of headings

--- js/heading-anchor.js
+++ js/heading-anchor.js
@@ -1,3 +1,3 @@
-document.querySelectorAll('h1,h2,h3').forEach(h => {
+document.querySelectorAll('h1,h2,h3,h4,h5,h6').forEach(h => {
   if (h.id) h.innerHTML += ` <span class="anchor"><a href="#${h.id}">#</a></span>`;
 });
