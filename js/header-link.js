e82481b9fa148c9462bd835de74fdedb1ce12885
Yihui Xie <xie@yihui.name>
2023-03-31 13:47:21 -0500

    modernize the js code

--- js/header-link.js
+++ js/header-link.js
@@ -1,8 +1,5 @@
 (function() {
-  var h, hs = document.querySelectorAll('h1,h2,h3');
-  for (var i = 0; i < hs.length; i++) {
-    h = hs[i];
-    if (h.id === '') continue;
-    h.innerHTML += ' <span class="anchor"><a href="#' + h.id + '">#</a></span>';
-  }
+  document.querySelectorAll('h1,h2,h3').forEach(h => {
+    if (h.id) h.innerHTML += ` <span class="anchor"><a href="#${h.id}">#</a></span>`;
+  });
 })();
