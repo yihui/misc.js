0cbde1c7a93a3b6bcf035ecee39eac0f737954dd
Yihui Xie <xie@yihui.name>
2023-04-06 09:29:29 -0500

    use string templates to simplify the code a bit

--- js/key-buttons.js
+++ js/key-buttons.js
@@ -1,8 +1,8 @@
-(function(d) {
+(d => {
   const a1 = ['Enter', 'Up', 'Down', 'Left', 'Right'];
   const a2 = ['&crarr;', '&uarr;', '&darr;', '&larr;', '&rarr;'];
   function drawArrows(x) {
-    a1.map((v, i) => x = x.replace(new RegExp('>' + v + '<', 'g'), ' title="' + v + (i ? ' Arrow' : '') + '">' + a2[i] + '<'));
+    a1.map((v, i) => x = x.replace(new RegExp(`>${v}<`, 'g'), ` title="${v + (i ? ' Arrow' : '')}">${a2[i]}<`));
     return x;
   }
   // 1. individual keys; 2. modifiers; 3. normal keys
@@ -10,14 +10,14 @@
               Array(12).fill().map((v, i) => 'F' + (i + 1)).concat(a1).join('|'),
         k2 = 'Ctrl|Control|Shift|Alt|Cmd|Command|fn',
         k3 = '[a-zA-Z0-9]|Click',
-        r1 = new RegExp('^(' + k1 + '|' + k2 + ')$'),
-        r2 = new RegExp('^(' + k2 + ') [/+] '),
-        r3 = new RegExp('^(' + [k1, k2, k3].join('|') + ')( [/+] )(.*)');
+        r1 = new RegExp(`^(${k1}|${k2})$`),
+        r2 = new RegExp(`^(${k2}) [/+] `),
+        r3 = new RegExp(`^(${k1}|${k2}|${k3})( [/+] )(.*)`);
   d.querySelectorAll(':not(pre) > code').forEach(el => {
     if (el.childElementCount > 0) return;
     let t = el.innerText;
     if (r1.test(t)) {
-      el.outerHTML = drawArrows('<kbd>' + t + '</kbd>');
+      el.outerHTML = drawArrows(`<kbd>${t}</kbd>`);
       return;
     }
     if (!r2.test(t)) return;
