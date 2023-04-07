e1cc392c489067eb536ad46ffe97a45eeb048657
Yihui Xie <xie@yihui.name>
2023-04-06 22:34:47 -0500

    a script to build TOC from headings

---
+++ js/toc.js
@@ -0,0 +1,49 @@
+// build TOC using headings
+(d => {
+  // find the body of the article
+  const b = d.querySelector('.article, .body, article, body');
+  if (!b) return;
+  const hs = b.querySelectorAll('h1, h2, h3, h4, h5, h6');
+  if (hs.length === 0) return;
+
+  var toc = b.getElementById('TOC');
+  toc?.remove();  // delete and rebuild TOC if it has been generated (e.g., by Pandoc)
+  toc = d.createElement('div');
+  toc.id = 'TOC';
+
+  let li, ul;
+  let p = toc;  // the current parent into which we insert child TOC items
+  let t1, t0 = 0;  // pretend there is a top-level <h0> for the sake of convenience
+  hs.forEach(h => {
+    t1 = parseInt(h.tagName.replace(/^h/i, ''));
+    li = d.createElement('li');
+    if (t1 > t0) {
+      // lower-level header: create a new ul
+      ul = d.createElement('ul');
+      ul.appendChild(li);
+      p.appendChild(ul);
+    } else if (t1 < t0) {
+      // higher-level header: go back to upper-level ul
+      for (let j = 0; j < t0 - t1; j++) {
+        p = p.parentNode.parentNode;
+      }
+    }
+    if (t1 <= t0) p.parentNode.appendChild(li);
+    p = li;
+    const a = d.createElement('a');
+    a.innerHTML = h.innerHTML;
+    if (h.id) {
+      a.href = '#' + h.id;
+    } else {
+      // Pandoc's section divs
+      const s = h.parentNode;
+      if (s.classList.contains('section') && s.id) a.href = '#' + s.id;
+    }
+    p.appendChild(a);
+    t0 = t1;
+  });
+  b.insertBefore(toc, b.firstChild);
+
+  // check if headings are numbered
+  toc.querySelector('span.section-number') && toc.classList.add('numbered');
+})(document);
