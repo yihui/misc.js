f8da1b313c9e50c4eb531f6b1c6c7c921912b706
Yihui Xie <xie@yihui.name>
2023-09-13 12:06:25 -0500

    don't try to shorten a link if it's not an external one

--- js/external-link.js
+++ js/external-link.js
@@ -1,10 +1,9 @@
 (function(d) {
   const r = /^(https?:)?\/\//;
   d.querySelectorAll('a').forEach(a => {
+    if (!r.test(a.getAttribute('href'))) return;
     // add _blank target to external links
-    if (r.test(a.getAttribute('href'))) {
-      a.target = '_blank';
-    }
+    a.target = '_blank';
     // shorten bare links
     if (a.childElementCount === 0) {
       a.innerText = a.innerText.replace(r, '').replace(/(.+)#.*$/, '$1');
