1917988f044ed6638a0acacb58a757de4d4f6f11
Yihui Xie <xie@yihui.name>
2023-09-13 13:21:23 -0500

    also treat `―` as the start character of a quote footer

--- js/right-quote.js
+++ js/right-quote.js
@@ -1,5 +1,5 @@
 // right-align a quote footer if it starts with ---
 [...document.getElementsByTagName('blockquote')].forEach(quote => {
   const el = quote.lastElementChild;
-  if (el?.tagName === 'P' && /^(—|---)/.test(el.textContent)) el.style.textAlign = 'right';
+  if (el?.tagName === 'P' && /^(—|―|---)/.test(el.textContent)) el.style.textAlign = 'right';
 });
