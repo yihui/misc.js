f925c1658bbee0e9792638338031b97d2a308eb2
Yihui Xie <xie@yihui.name>
2023-09-18 22:42:58 -0500

    the js code can be greatly simplifed by a proper selector

--- js/no-highlight.js
+++ js/no-highlight.js
@@ -1,15 +1,8 @@
-(function() {
-  var i, cls, code, codes = document.getElementsByTagName('pre');
-  for (i = 0; i < codes.length; i++) {
-    code = codes[i];
-    if (code.children.length !== 1) continue;
-    code = code.children[0];
-    if (code.tagName !== 'CODE') continue;
-    cls = code.className;
-    if (cls === '' || cls === 'hljs') {
-      code.className = 'nohighlight';
-    } else if (/^language-/.test(cls) && !/hljs/.test(cls)) {
-      code.className += ' hljs';
-    }
+document.querySelectorAll('pre > code:only-child').forEach(code => {
+  const cls = code.className;
+  if (cls === '' || cls === 'hljs') {
+    code.className = 'nohighlight';
+  } else if (/^language-/.test(cls) && !/hljs/.test(cls)) {
+    code.className += ' hljs';
   }
-})();
+})
