9951d6adb95496ed6c5b66b17a923b1cca3cfc5f
Yihui Xie <xie@yihui.name>
2023-10-13 10:00:19 -0500

    support converting <uL> after a .tabset element into a tabset; I think this is actually more natural than using headings

--- js/tabsets.js
+++ js/tabsets.js
@@ -1,20 +1,39 @@
-// find an element with class `tabset` and convert its subsequent headings to tabs;
+// find an element with class `tabset` and convert its subsequent bullet list or headings to tabs;
 // see documentation at: https://yihui.org/en/2023/10/section-tabsets/
 document.querySelectorAll('.tabset').forEach(h => {
-  let links = h.querySelectorAll(':scope > .tab-link'),
-      panes = h.querySelectorAll(':scope > .tab-pane');
-  // using headings to create a tabset later if it's empty
-  if (links.length === 0) {
-    links = []; panes = [];
-  }
+  const links = [...h.querySelectorAll(':scope > .tab-link')],
+        panes = [...h.querySelectorAll(':scope > .tab-pane')];
   function activate(i) {
     function a(x, i) {
       x.forEach((el, k) => el.classList[k === i ? 'add' : 'remove']('active'));
     }
     a(links, i); a(panes, i);
   }
-  let n = -1, el = h.nextSibling, p;
-  if (links instanceof Array) while (el) {
+  function newEl(tag, cls) {
+    const el = document.createElement(tag);
+    el.className = cls;
+    return el;
+  }
+  let n = -1, el = h.nextElementSibling, p;
+  // if the first sibling is <ul>, try to convert it to tabset
+  if (links.length === 0 && el.tagName === 'UL') {
+    [...el.children].forEach(li => {
+      const l = li.firstElementChild;
+      if (!l) return;
+      const l2 = newEl('div', 'tab-link');
+      l2.append(l);
+      l.outerHTML = l.innerHTML;
+      el.before(l2);
+      const p = newEl('div', 'tab-pane');
+      l2.after(p);
+      [...li.children].forEach(l => p.append(l));
+      links.push(l2); panes.push(p);
+    });
+    el.remove();
+  }
+  // create a tabset using headings if the above didn't work
+  if (links.length === 0) while (el) {
+    // convert headings to tabs
     if (el.nodeName === '#comment' && el.nodeValue.trim() === `tabset:${h.id}`)
       break;  // quit after <!--tabset:id-->
     const t = el.tagName;
@@ -24,8 +43,7 @@ document.querySelectorAll('.tabset').forEach(h => {
       if (n < 0) n = n2 - 1;
       // find the next lower-level heading and start creating a tab
       if (n2 === n + 1) {
-        p = document.createElement('div');
-        p.className = 'tab-pane';
+        p = newEl('div', 'tab-pane');
         el.after(p);
         el.classList.add('tab-link');
         el.querySelector('.anchor')?.remove();
@@ -41,6 +59,11 @@ document.querySelectorAll('.tabset').forEach(h => {
       el = p;
     }
     el = el.nextSibling;
+  }
+  // if the initial tabset container is empty, move links and panes into it
+  if (h.innerText.trim() == '') {
+    links.forEach(l => h.append(l));
+    panes.forEach(p => h.append(p));
   }
   // activate one tab initially if none is active
   let init = 0;
