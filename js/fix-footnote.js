d7a05497e1e2842c2bf3f6621f358bfe242c4121
Yihui Xie <xie@yihui.name>
2023-10-19 09:58:36 -0500

    add the space before the footnote number element instead of inside it

--- js/fix-footnote.js
+++ js/fix-footnote.js
@@ -1,7 +1,9 @@
 (d => {
   // add [] to footnote numbers
   d.querySelectorAll('sup[id^="fnref:"] > a.footnote-ref, a.footnote-ref > sup').forEach(el => {
-    if (/^[0-9]+$/.test(el.innerText)) el.innerText = ' [' + el.innerText + ']';
+    if (!/^[0-9]+$/.test(el.innerText)) return;
+    el.innerText = `[${el.innerText}]`;
+    el.before(d.createTextNode(' '));
   });
   // move the return symbol into the previous <p>
   d.querySelectorAll('.footnotes > ol > li > p ~ .footnote-return').forEach(el => {
