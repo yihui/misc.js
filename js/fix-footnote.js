f0a21f184f38cabf0cf70e862e6d2181e529895e
Yihui Xie <xie@yihui.name>
2023-10-19 10:02:43 -0500

    the space should be added before the parent node

--- js/fix-footnote.js
+++ js/fix-footnote.js
@@ -3,7 +3,7 @@
   d.querySelectorAll('sup[id^="fnref:"] > a.footnote-ref, a.footnote-ref > sup').forEach(el => {
     if (!/^[0-9]+$/.test(el.innerText)) return;
     el.innerText = `[${el.innerText}]`;
-    el.before(d.createTextNode(' '));
+    el.parentNode.before(d.createTextNode(' '));
   });
   // move the return symbol into the previous <p>
   d.querySelectorAll('.footnotes > ol > li > p ~ .footnote-return').forEach(el => {
