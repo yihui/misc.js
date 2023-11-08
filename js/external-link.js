544304739c6a257bb3e620a382825987b5fb88d8
Yihui Xie <xie@yihui.name>
2023-11-08 12:44:24 -0600

    add the title attribute to <a> if not exists, and the value is the href, so readers know the destination of a link clearly when hovering over the link
    web browsers can be slow to show the URL in the status bar, and sometimes shows an incomplete URL there, too

--- js/external-link.js
+++ js/external-link.js
@@ -1,6 +1,7 @@
 (d => {
   const r = /^(https?:)?\/\//;
   d.querySelectorAll('a').forEach(a => {
+    if (!a.title) a.title = a.href;
     if (!r.test(a.getAttribute('href'))) return;
     // add _blank target to external links
     a.target = '_blank';
