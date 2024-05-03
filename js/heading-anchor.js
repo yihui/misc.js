66a4e0a1abe05f44deaf09bb39759093b4a6e918
Yihui Xie <xie@yihui.name>
2024-05-03 08:10:32 -0500

    don't add anchor if it already exists in heading; also use .insertAdjacentHTML() instead of .innerHTML to not destroy potential event listeners on heading

--- js/heading-anchor.js
+++ js/heading-anchor.js
@@ -1,3 +1,4 @@
 document.querySelectorAll('h1,h2,h3,h4,h5,h6').forEach(h => {
-  if (h.id) h.innerHTML += ` <span class="anchor"><a href="#${h.id}">#</a></span>`;
+  if (h.id && !h.querySelector('.anchor'))
+    h.insertAdjacentHTML('beforeend', ` <span class="anchor"><a href="#${h.id}">#</a></span>`);
 });
