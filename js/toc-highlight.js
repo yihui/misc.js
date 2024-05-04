4674c61bcc50582dab9925fbf04b092afa12c44d
Yihui Xie <xie@yihui.name>
2024-05-04 16:45:33 -0500

    significantly enhanced the scroll-highlighting and also added support for opening/closing parent TOC items that do not contain the active item

--- js/toc-highlight.js
+++ js/toc-highlight.js
@@ -5,17 +5,27 @@
   if (!toc) return;
   const links = toc.querySelectorAll('a');
   if (!links.length) return;
+  const lis = toc.querySelectorAll('li');
 
+  // record which elements are currently in the viewport
+  const ids = [];
   // create a new Intersection Observer instance
   const observer = new IntersectionObserver(els => els.forEach(el => {
-    const id = el.target.id;
-    id && el.isIntersecting && links.forEach(a => {
-      a.classList[(a.getAttribute('href') === '#' + id) ? 'add' : 'remove']('active');
+    const id = el.target.id, i = ids.indexOf(id);
+    el.isIntersecting ? ids.push(id) : (i > -1 && ids.splice(i, 1));
+    const n = ids.length;
+    if (!n) return;
+    links.forEach(a => {
+      const action = (a.getAttribute('href') === '#' + ids[n - 1])  ? 'add' : 'remove';
+      a.classList[action]('active');
+    });
+    lis.forEach(li => {
+      li.classList[li.querySelector('.active') ? 'add' : 'remove']('open');
     });
   }));
 
   // observe all section headings
   d.querySelectorAll('h1,h2,h3,h4,h5,h6').forEach(h => {
-    h.nodeType === 1 && observer.observe(h);
+    h.nodeType === 1 && h.id && observer.observe(h);
   });
 })(document);
