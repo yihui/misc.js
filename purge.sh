4597d9236fa42010f8d985b37bf757baf5f4d983
Yihui Xie <xie@yihui.name>
2024-08-24 10:17:16 -0500

    github.event.before doesn't seem to work; use tags instead

--- purge.sh
+++ purge.sh
@@ -3,7 +3,9 @@
 # purge jsdevlivr cache for assets modified between current and previous tags
 JSD_URL="https://purge.jsdelivr.net/npm/@xiee/utils"
 
-for i in $(git diff --name-only $PREVIOUS_COMMIT $CURRENT_COMMIT | grep -E "[.](css|js)$"); do
+tag2=$(git describe --tags --abbrev=0)
+tag1=$(git describe --tags --abbrev=0 "$tag2^")
+for i in $(git diff --name-only $tag1 $tag2 | grep -E "[.](css|js)$"); do
   curl "${JSD_URL}/${i}"
   curl "${JSD_URL}/${i%.*}.min.${i##.}"
 done
