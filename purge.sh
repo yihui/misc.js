ba57b528baaf06809fbe564fb8e7abedded17b4c
Yihui Xie <xie@yihui.name>
2024-08-24 09:57:10 -0500

    fetch full history so purge.sh can find commit from previous push

--- purge.sh
+++ purge.sh
@@ -3,9 +3,7 @@
 # purge jsdevlivr cache for assets modified between current and previous tags
 JSD_URL="https://purge.jsdelivr.net/npm/@xiee/utils"
 
-tag2=$(git describe --tags --abbrev=0)
-tag1=$(git describe --tags --abbrev=0 "$tag2^")
-for i in $(git diff --name-only $tag1 $tag2 | grep -E "[.](css|js)$"); do
+for i in $(git diff --name-only $PREVIOUS_COMMIT $CURRENT_COMMIT | grep -E "[.](css|js)$"); do
   curl "${JSD_URL}/${i}"
   curl "${JSD_URL}/${i%.*}.min.${i##.}"
 done
