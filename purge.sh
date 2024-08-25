ebf8ccda99356670cf5621fb5e2ec60c4ebfd1c5
Yihui Xie <xie@yihui.name>
2024-08-24 22:23:54 -0500

    exclude empty $i

--- purge.sh
+++ purge.sh
@@ -6,6 +6,8 @@ JSD_URL="https://purge.jsdelivr.net/npm/@xiee/utils"
 tag2=$(git describe --tags --abbrev=0)
 tag1=$(git describe --tags --abbrev=0 "$tag2^")
 for i in $(git diff --name-only $tag1 $tag2 | grep -E "[.](css|js)$"); do
-  curl "${JSD_URL}/${i}"
-  curl "${JSD_URL}/$(echo "$i" | sed 's/(\.[^.]*$)/.min\1/')"
+  if [ -n "$i" ]; then
+    curl "${JSD_URL}/${i}"
+    curl "${JSD_URL}/$(echo "$i" | sed 's/(\.[^.]*$)/.min\1/')"
+  fi
 done
