99a532f016f8e9a93315026d57517245ed473dd1
Yihui Xie <xie@yihui.name>
2024-10-03 17:17:44 -0500

    rewrite callouts to make it easier to redefine both legend text and icon globally
    previously the legend text was hard-coded; now it's defined in css via `content: attr(data-legend)`, which means we could do something like
    
      .callout-tip legend::after { content: "提示"; }
    
    to translate 'tip' to Chinese for the whole document

--- js/callout.js
+++ js/callout.js
@@ -1,10 +1,10 @@
 // turn <div class="callout-*"> to <fieldset><legend>*
 document.querySelectorAll('div[class^="callout-"]').forEach(el => {
   const f = document.createElement('fieldset');
-  f.className = el.className;
+  f.className = el.className; f.classList.add('callout');
   // if the data-legend attribute exists, use its value as legend, otherwise use the class name
-  const l = el.dataset.legend || el.classList[0].replace('callout-', '');
-  f.insertAdjacentHTML('afterbegin', `<legend>${l.toUpperCase()}</legend>`);
+  f.insertAdjacentHTML('afterbegin', '<legend></legend>');
+  f.firstChild.dataset.legend = el.dataset.legend || el.classList[0].replace('callout-', '');
   el.after(f);
   f.append(...el.children);
   el.remove();
