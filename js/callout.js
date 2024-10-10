c93f75fb7ff78d9ef001f7db2dba9926c6fa7d0e
Yihui Xie <xie@yihui.name>
2024-10-10 09:19:49 -0500

    if the callout class has multiple words separated by -, replace - with space before using the class name as the legend title

--- js/callout.js
+++ js/callout.js
@@ -4,7 +4,7 @@ document.querySelectorAll('div[class^="callout-"]').forEach(el => {
   f.className = el.className; f.classList.add('callout');
   // if the data-legend attribute exists, use its value as legend, otherwise use the class name
   f.insertAdjacentHTML('afterbegin', '<legend></legend>');
-  f.firstChild.dataset.legend = el.dataset.legend || el.classList[0].replace('callout-', '');
+  f.firstChild.dataset.legend = el.dataset.legend || el.classList[0].replace('callout-', '').replace('-', ' ');
   el.after(f);
   f.append(...el.children);
   el.remove();
